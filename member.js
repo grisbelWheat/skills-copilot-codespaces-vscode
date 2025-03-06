061f163f1d046a8ce2e56cce23c193f53a457a47
grisbelWheat <carlosdanielhuancafarfan01@gmail.com>
2025-03-06 22:11:29 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,11 @@
+function skillsMember() {
+    const member = {
+        name: 'John Doe',
+        age: 25,
+        skills: ['html', 'css', 'js'],
+        greet: function() {
+            console.log('Hello');
+        },
+    };
+    return member;
+}
